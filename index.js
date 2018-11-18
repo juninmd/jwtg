d5087bd6460ebff92ef02688eca4fba232eaf484
Antonio Carlos <jr_acn@yahoo.com.br>
2018-11-18 16:33:49 -0200

    Validation

--- index.js
+++ index.js
@@ -2,19 +2,27 @@
 
 const jsonwebtoken = require('jsonwebtoken');
 const fs = require('fs');
-
+const pathLib = require('path');
 
 (() => {
-    const [_, _jwtg, secretKey, path] = process.argv;
+    let [_, _jwtg, secretKey, path] = process.argv;
 
     let json = {};
 
     if (path) {
-        if (fs.existsSync(path)) {
-            json = require(path);
+        path = pathLib.resolve(path);
+
+        if (fs.existsSync(path) && pathLib.extname(path) === '.json') {
+            try {
+                json = JSON.parse(fs.readFileSync(path).toString());
+            } catch (error) {
+                console.error('Invalid JSON:', error.message);
+                console.error('Path:', path);
+                return;
+            }
         }
         else {
-            console.error('Please, check your path. File not found!');
+            console.error('Please, check your path, and select a valid JSON File!');
             console.error('Path:', path);
             return;
         }

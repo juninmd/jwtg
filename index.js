6e3e287409f36c7eb4808340011ce19e40f9f33d
Antonio <antonio.junior@luizalabs.com>
2019-11-19 17:57:11 -0300

    v2

--- index.js
+++ index.js
@@ -4,40 +4,51 @@ const jsonwebtoken = require('jsonwebtoken');
 const fs = require('fs');
 const pathLib = require('path');
 const clipboardy = require('clipboardy');
+const program = require('commander');
 
-(() => {
-    let [_, _jwtg, secretKey, path] = process.argv;
 
-    let json = {};
+program
+    .option('-p, --path', 'Localização do arquivo json')
+    .option('-s, --secret <value>', 'Valor do secret');
 
-    if (path) {
-        path = pathLib.resolve(path);
+program
+    .command('generate') // sub-command name
+    .alias('g') // alternative sub-command is `al`
+    .description('Gera token do jwtg') // command description
+    .action(() => {
+        let [_, _jwtg, secretKey, path] = process.argv;
 
-        if (fs.existsSync(path) && pathLib.extname(path) === '.json') {
-            try {
-                json = JSON.parse(fs.readFileSync(path).toString());
-            } catch (error) {
-                console.error('Invalid JSON:', error.message);
+        let json = {};
+
+        if (path) {
+            path = pathLib.resolve(path);
+
+            if (fs.existsSync(path) && pathLib.extname(path) === '.json') {
+                try {
+                    json = JSON.parse(fs.readFileSync(path).toString());
+                } catch (error) {
+                    console.error('Invalid JSON:', error.message);
+                    console.error('Path:', path);
+                    return;
+                }
+            }
+            else {
+                console.error('Please, check your path, and select a valid JSON File!');
                 console.error('Path:', path);
                 return;
             }
         }
-        else {
-            console.error('Please, check your path, and select a valid JSON File!');
-            console.error('Path:', path);
-            return;
-        }
-    }
 
-    const token = jsonwebtoken.sign(json, secretKey || 'stubJWT', {
-        noTimestamp: true
+        const token = jsonwebtoken.sign(json, secretKey || 'stubJWT', {
+            noTimestamp: true
+        });
+
+        console.log(token);
+
+        clipboardy.writeSync(token);
+
+        console.log('JSON:', json);
+        console.log('Secret:', secretKey || 'stubJWT');
     });
-
-    console.log(token);
-
-    clipboardy.writeSync(token);
-
-    console.log('JSON:', json);
-    console.log('Secret:', secretKey || 'stubJWT');
-})();
-
+// allow commander to parse `process.argv`
+program.parse(process.argv);
